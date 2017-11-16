c5c3015ba3d607e855537cb99b98c94d7c976fc1
Autophagy <mika@crate.io>
2017-11-16 14:10:23 +0100

    Added setup.py stuff

--- datarum/__init__.py
+++ datarum/__init__.py
@@ -1,2 +1,6 @@
 from .wending import wending
 from .converter import from_date, today
+
+
+__version__ = '0.1.0'
+__version_full__ = __version__
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from datarum import __version__
+
+setup(
+    name='datarum',
+    version=__version__,
+    author='Mika Naylor (Autophagy)',
+    author_email='mail@autophagy.io',
+    url='https://github.com/Autophagy/datarum',
+    description=('Datárum is a small python library to convert Gregorian '
+                 'dates to Wending, an Old English variant on the French '
+                 'Republican calendar, for use in various projects.'),
+    long_description=open('README.rst', encoding='utf-8').read(),
+    packages=['datarum'],
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
