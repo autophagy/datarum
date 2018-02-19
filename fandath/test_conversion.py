25af0bea6494f9c828809652d5ff965df1ff344e
Autophagy <mika@crate.io>
2018-02-19 11:39:37 +0100

    Remove testing for early leaps
    I dont really care if the first few years are accurate with leaps, and having to abide by them is complicated. Will just use the romme 4/100/400 rule from now on for simplification and optimisation purposes

--- fandath/test_conversion.py
+++ fandath/test_conversion.py
@@ -4,23 +4,12 @@ from datetime import datetime
 
 
 year_starts = [
-    ((10, 1, 1, 16, 45, 10), (1801, 9, 23, 16, 45, 10)),
-    ((11, 1, 1, 16, 45, 10), (1802, 9, 23, 16, 45, 10)),
-    ((12, 1, 1, 16, 45, 10), (1803, 9, 24, 16, 45, 10)),
-    ((13, 1, 1, 16, 45, 10), (1804, 9, 23, 16, 45, 10)),
-    ((14, 1, 1, 16, 45, 10), (1805, 9, 23, 16, 45, 10)),
     ((223, 1, 1, 16, 45, 10), (2014, 9, 22, 16, 45, 10)),
     ((224, 1, 1, 16, 45, 10), (2015, 9, 22, 16, 45, 10)),
     ((225, 1, 1, 16, 45, 10), (2016, 9, 22, 16, 45, 10)),
     ((226, 1, 1, 16, 45, 10), (2017, 9, 22, 16, 45, 10)),
     ((227, 1, 1, 16, 45, 10), (2018, 9, 22, 16, 45, 10)),
     ((228, 1, 1, 16, 45, 10), (2019, 9, 22, 16, 45, 10)),
-]
-
-leaps = [
-    ((3, 13, 6, 16, 45, 10), (1795, 9, 22, 16, 45, 10)),
-    ((7, 13, 6, 16, 45, 10), (1799, 9, 22, 16, 45, 10)),
-    ((11, 13, 6, 16, 45, 10), (1803, 9, 23, 16, 45, 10)),
 ]
 
 romme = [
@@ -44,12 +33,6 @@ class TestWendingConversion(unittest.TestCase):
             c = from_date(gregorian_date)
             self.assertEqual(dat, c.tuple())
 
-    def test_leaps(self):
-        for dat, gregorian in leaps:
-            gregorian_date = datetime(*gregorian)
-            c = from_date(gregorian_date)
-            self.assertEqual(dat, c.tuple())
-
     def test_romme_leaps(self):
         for dat, gregorian in romme:
             gregorian_date = datetime(*gregorian)
@@ -61,13 +44,6 @@ class TestGregorianConversion(unittest.TestCase):
 
     def test_year_starts(self):
         for dat, gregorian in year_starts:
-            wending_date = wending(*dat)
-            g = to_gregorian(wending_date)
-            self.assertEqual(gregorian, (g.year, g.month, g.day, g.hour,
-                                         g.minute, g.second))
-
-    def test_leaps(self):
-        for dat, gregorian in leaps:
             wending_date = wending(*dat)
             g = to_gregorian(wending_date)
             self.assertEqual(gregorian, (g.year, g.month, g.day, g.hour,
